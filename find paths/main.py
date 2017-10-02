7d7e35da731a5b70e442d62fd3f6adf3e6115e4e
Muhammad Saood Najib <mnajib@hamilton.edu>
2017-10-02 06:43:11 -0400

    Update main.py

--- find paths/main.py
+++ find paths/main.py
@@ -1,7 +1,5 @@
 def krakenCount(m,n):
 	Grid = [[0 for col in range(n)] for row in range(m)]
-
-
 	for col in range(n):
 		Grid[0][col] = 1
 
@@ -12,8 +10,7 @@ def krakenCount(m,n):
 		for col in range(n):
 			if(row == col):
 				Grid[row][col] = 1
-
-
+				
 	for row in range(1,m):
 		for col in range(1,n):
 			Grid[row][col] = Grid[row-1][col] + Grid[row][col-1] + Grid[row-1][col-1]
